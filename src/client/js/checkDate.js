03fa7eb33f89dde9ec53beab6b0d28d8e5db8792
IsiahDB <IsaiahBanks500@gmail.com>
2021-07-30 16:30:58 -0500

    init commit

--- src/client/js/checkDate.js
+++ src/client/js/checkDate.js
@@ -1,18 +1,17 @@
 
 export function checkDate(LeaveDate,ReturnDate) {
-    try {
-        let lDate = new Date(LeaveDate)
-        let rDate = new Date(ReturnDate)
-        let timeDiff = Math.abs(rDate - lDate)
-        const dateCalc = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); 
-        console.log(dateCalc + " days");
-    } catch(error){
-        if(dateCalc >= 7){
-            console.log('Trip must be within 7 days',error)
-        } else{
-            return dateCalc
-        }
+    
+    let lDate = new Date(LeaveDate)
+    let rDate = new Date(ReturnDate)
+    let timeDiff = Math.abs(rDate - lDate)
+    const dateCalc = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); 
+    console.log(dateCalc + " days");
 
+    if(dateCalc >= 7){
+        console.log('Trip must be within 7 days',error)
+    } else{
+        return dateCalc
     }
+
     
 }
